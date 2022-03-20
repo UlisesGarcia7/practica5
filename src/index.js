290272a2bd61feca2ea77d6b3092eab41a45ffdf
Ulises Garcia <2019030286@upsin.edu.mx>
2022-03-20 16:55:44 -0600

    Practica5 Agregada

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Manda A Llamar el Contenido De La Constante
+import Datos from './Componente';
+
+// Imprimir Los Valores De La Tabla
+ReactDOM.render(
+  <Datos />,
+  document.getElementById('root')
+);
+
